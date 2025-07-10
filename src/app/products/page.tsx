5c4e0e36245d0f7532c44a5960e18b793ee8a3b0
MINJEONG <lxxmnmn@gmail.com>
2025-07-10 18:06:36 +0900

    feat: 상품 목록 페이지 라우팅

---
+++ src/app/products/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+export default function ProductListPage() {
+  return (
+    <main>
+      <h1>상품 목록</h1>
+      <section>
+        <ul>
+          <li></li>
+        </ul>
+      </section>
+    </main>
+  );
+}
