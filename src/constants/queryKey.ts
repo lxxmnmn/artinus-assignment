88142b279f24eb2c7bc98b36f54ee3116004a753
MINJEONG <lxxmnmn@gmail.com>
2025-07-13 16:08:14 +0900

    refactor: 쿼리키 상수 분리

---
+++ src/constants/queryKey.ts
@@ -0,0 +1,4 @@
+export const PRODUCT_QUERY_KEY = {
+  LIST: 'productList',
+  DETAIL: 'productDetail',
+} as const;
--- src/hooks/useProduct.ts
+++ src/hooks/useProduct.ts
@@ -1,11 +1,12 @@
 import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 import { fetchProductList, fetchProductDetail } from '@/lib/api/product';
+import { PRODUCT_QUERY_KEY } from '@/constants/queryKey';
 
 const LIMIT = 20 as const;
 
 export const useInfiniteProductList = () => {
   return useInfiniteQuery({
-    queryKey: ['productList'],
+    queryKey: [PRODUCT_QUERY_KEY.LIST],
     queryFn: ({ pageParam = 0 }) => fetchProductList(pageParam, LIMIT),
     getNextPageParam: (lastPage, allPages) => {
       const loadedCount = allPages.flatMap((page) => page.products).length;
@@ -17,7 +18,7 @@ export const useInfiniteProductList = () => {
 
 export const useProductDetail = (id: number) => {
   return useQuery({
-    queryKey: ['productDetail', id],
+    queryKey: [PRODUCT_QUERY_KEY.DETAIL, id],
     queryFn: () => fetchProductDetail(id),
     enabled: !!id,
   });
