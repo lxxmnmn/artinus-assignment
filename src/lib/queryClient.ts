9a5c4750d43ea638ba84b69fce43d8a5fc781ab4
MINJEONG <lxxmnmn@gmail.com>
2025-07-14 08:45:52 +0900

    chore: QueryClient 옵션 설정

--- src/lib/queryClient.ts
+++ src/lib/queryClient.ts
@@ -1,5 +1,12 @@
 import { QueryClient } from '@tanstack/react-query';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      gcTime: 1000 * 60 * 10,
+    },
+  },
+});
 
 export default queryClient;
