6f73706eed1c24595a92c7a1b56814220ad2d39f
MINJEONG <lxxmnmn@gmail.com>
2025-07-14 18:35:18 +0900

    style: UI 스타일링 수정

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -34,10 +34,12 @@ export default function ProductListPage() {
   const products: Product[] = data?.pages.flatMap((page) => page.products) ?? [];
 
   return (
-    <main className="max-w-6xl mx-auto p-10 py-14">
-      <h1 className="text-3xl font-extrabold mb-8">Products</h1>
+    <main className="max-w-7xl mx-auto px-12 py-14">
+      <h1 className="sticky top-0 z-1 mb-10 px-2 py-4 bg-white border-b-1 border-b-neutral-200 text-3xl font-extrabold">
+        Products
+      </h1>
 
-      <section>
+      <section className="px-3">
         <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
           {products.map((product, i) => (
             <li key={product.id}>
@@ -48,7 +50,7 @@ export default function ProductListPage() {
           ))}
         </ul>
 
-        <div ref={lazyLoadRef} className="min-h-[80px] flex justify-center items-center mt-8">
+        <div ref={lazyLoadRef} className="min-h-[60px] flex justify-center items-center mt-10">
           {(!products.length || isFetchingNextPage) && (
             <LoaderCircle
               role="status"
